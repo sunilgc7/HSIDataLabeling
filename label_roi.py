d01dd260df90fc675b97bec5c97f6232be90c1bc
sunil <sgc>
2024-07-02 09:35:38 -0500

    test

---
+++ label_roi.py
@@ -0,0 +1,7 @@
+from src.common.read_hsi_image_file import *
+
+image_dir_path = "/home/developer/SGCPostDOC/data/sample/1_G121R3_2024-04-02_18-14-49"
+
+label_image_folder = "/home/developer/SGCPostDOC/data/labeled"
+read_hsi(image_dir_path,label_image_folder)
+
--- src/utils/roi_selection.py
+++
@@ -1,7 +0,0 @@
-
-def gui():
-    # gui showing a pudo rgb image for roi selection
-    print()
-
-def get_psuedo_rgb():
-    print()
