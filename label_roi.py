2ba0035b9d78d510a3afe4edef30131013e487b3
ubuntusgc <ubuntusgc>
2024-07-09 12:49:34 -0500

    test

--- label_roi.py
+++ label_roi.py
@@ -1,7 +1,7 @@
 from src.common.read_hsi_image_file import *
 
-image_dir_path = "/home/developer/SGCPostDOC/data/sample/1_G121R3_2024-04-02_18-14-49"
+image_dir_path = "/home/pagsun/SunilGCData/2_normal_system/camelina/after acclamatization/HSI/10_G116R4_2024-03-25_17-58-58"
 
-label_image_folder = "/home/developer/SGCPostDOC/data/labeled"
+label_image_folder = "/home/pagsun/SunilGCData/2_normal_system/camelina/labeled"
 read_hsi(image_dir_path,label_image_folder)
 
--- unittest/test_gui_roi_selection.py
+++ unittest/test_gui_roi_selection.py
@@ -2,7 +2,7 @@ import pytest
 from scipy import stats
 from ..src.common.read_hsi_image_file import *
 
-image_dir_path = "/home/developer/SGCPostDOC/data/sample/1_G121R3_2024-04-02_18-14-49"
+image_dir_path = "/home/pagsun/SunilGCData/2_normal_system/camelina/after acclamatization/HSI/10_G116R4_2024-03-25_17-58-58"
 
 # pytest test_example.py
 
@@ -11,5 +11,5 @@ def test_get_hsi_image(image_dir_path):
   """
   This test compares the means of two treatment groups using a two-tailed t-test.
   """
-  label_image_folder = "/home/developer/SGCPostDOC/data/labeled"
+  label_image_folder = "/home/pagsun/SunilGCData/camelina_label_data"
   read_hsi(image_dir_path,label_image_folder)
