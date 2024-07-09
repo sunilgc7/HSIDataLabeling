0735911eedb4607a15481166b1fb2590a5edff3e
sunil <sgc>
2024-07-09 12:53:58 -0500

    test

--- label_roi.py
+++ label_roi.py
@@ -1,7 +1,21 @@
 from src.common.read_hsi_image_file import *
+import argparse
 
-image_dir_path = "/home/pagsun/SunilGCData/2_normal_system/camelina/after acclamatization/HSI/10_G116R4_2024-03-25_17-58-58"
+parser = argparse.ArgumentParser(
+        description="DL segmentation model training")
+parser.add_argument("-p",
+                        "--im_path",
+                        help="Path for HSI image",
+                        type=str)
+parser.add_argument("-l",
+                        "--lbl_path",
+                        help="Label path",
+                        type=str)
+    
+args = parser.parse_args()
 
-label_image_folder = "/home/pagsun/SunilGCData/2_normal_system/camelina/labeled"
+image_dir_path = args.im_path
+label_image_folder = args.lbl_path
+
 read_hsi(image_dir_path,label_image_folder)
 
