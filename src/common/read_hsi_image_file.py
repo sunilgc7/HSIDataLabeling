6b1e5f917439b1ec8dc88abd593734a041e4be43
sunil <sgc>
2024-08-08 11:22:54 -0500

    test

--- src/common/read_hsi_image_file.py
+++ src/common/read_hsi_image_file.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 from ..utils.rectangle_drawer import *
+from plantcv import plantcv as pcv
 
 def read_hsi(image_dir_path,label_dir):
     print("*******************************")
@@ -12,22 +13,18 @@ def read_hsi(image_dir_path,label_dir):
     filtered_list = [x for x in dir_list if "png" in x]
     folder = filtered_list[0].split(".")[0]
 
-    dark_ref_hdr_path = image_dir_path+"/capture/DARKREF_"+folder+".hdr"
     dark_ref_raw_path = image_dir_path+"/capture/DARKREF_"+folder+".raw"
 
-    white_ref_hdr_path = image_dir_path+"/capture/WHITEREF_"+folder+".hdr"
-    whie_ref_raw_path = image_dir_path+"/capture/WHITEREF_"+folder+".raw"
+    white_ref_raw_path = image_dir_path+"/capture/WHITEREF_"+folder+".raw"
 
-    data_hdr_path = image_dir_path+"/capture/"+folder+".hdr"
     data_raw_path = image_dir_path+"/capture/"+folder+".raw"
 
-    dark_ref = envi.open(dark_ref_hdr_path, dark_ref_raw_path)
-    white_ref = envi.open(white_ref_hdr_path, whie_ref_raw_path)
-    data_ref = envi.open(data_hdr_path, data_raw_path)
-
-    white_ref_array = np.array(white_ref.load())
-    dark_ref_array = np.array(dark_ref.load())
-    data_ref_array = np.array(data_ref.load())
+    raw_img        = pcv.readimage(data_raw_path, mode='envi')
+    white_ref      = pcv.readimage(white_ref_raw_path, mode='envi')
+    dark_ref       = pcv.readimage(dark_ref_raw_path, mode='envi')
+    
+    calibrateddata = pcv.hyperspectral.calibrate(raw_data = raw_img, white_reference= white_ref, dark_reference= dark_ref)
+    data_ref_array = calibrateddata.array_data
 
     blue_band = data_ref_array[..., 24]
     green_band = data_ref_array[..., 70]
